c193d1008dcd948f296ce2f075230e20f28f3d69
Oleg Smetanin <olegsmith@gmail.com>
2014-09-12 14:18:53 +0400

    add UACadastreIdentify

--- components/leaflet.geomanager/providers/uacadastre.js
+++ components/leaflet.geomanager/providers/uacadastre.js
@@ -1,14 +1,125 @@
+L.GeoManager.UACadastreIdentify = function (options) {
+
+    var that = this;
+
+    var unproject = function (x, y) {
+        var earthRadius = 6378137;
+        return L.CRS.EPSG900913.projection.unproject((new L.Point(x, y)).divideBy(earthRadius));
+    };
+
+    var dfd = new jQuery.Deferred();
+
+    var identify = function (latlng) {
+
+        var dfdid = new jQuery.Deferred();
+
+        var map = options.map
+            , mapBounds = map.getBounds()
+            , sw = L.CRS.EPSG900913.project(mapBounds.getSouthWest())
+            , ne = L.CRS.EPSG900913.project(mapBounds.getNorthEast())
+            , xy = map.latLngToContainerPoint(latlng)
+            , bbox = sw.x + ',' + sw.y + ',' + ne.x + ',' + ne.y
+            , size = map.getSize();
+
+        $.ajax({
+            url : 'http://212.26.144.110/geowebcache/service/wms'
+            , dataType : 'xml'
+            , data : {
+                'tiled': true
+                , 'LAYERS': 'kadastr'
+                , 'QUERY_LAYERS': 'kadastr'
+                , 'STYLES': ''
+                , 'SERVICE': 'WMS'
+                , 'VERSION': '1.1.1'
+                , 'REQUEST': 'GetFeatureInfo'
+                , 'BBOX' : bbox
+                , 'FEATURE_COUNT': '1'
+                , 'WIDTH': size.x
+                , 'HEIGHT': size.y  
+                , 'FORMAT': 'image/png'
+                , 'INFO_FORMAT': 'application/vnd.ogc.gml'
+                , 'SRS': 'EPSG:900913'
+                , 'X': xy.x
+                , 'Y': xy.y
+            }
+        })
+        .done(function(data){
+            if (data) {
+                var $xml = $(data)
+                    , original_coords = $xml.find('gml\\:coordinates').text().split(' ')
+                    , koatuu = $xml.find('dzk\\:koatuu').text()       
+                    , zone = $xml.find('dzk\\:zona').text()  
+                    , quartal = $xml.find('dzk\\:kvartal').text() 
+                    , parcel = $xml.find('dzk\\:parcel').text()   
+                    , coords = [];
+
+                for (var i = 0; i < original_coords.length; i++) {
+
+                    var coord = original_coords[i].split(',')
+                        , unprjected_coord = unproject(coord[0], coord[1]);
+
+                    coords.push(unprjected_coord);
+                }
+                    
+                var poly = new L.Polygon(coords);   
+
+
+                $.ajax({       
+                    url : 'http://212.26.144.110/kadastrova-karta/get-parcel-Info'
+                    , type: 'GET'
+                    , dataType : 'json'
+                    , data : {
+                        'koatuu' : koatuu,
+                        'zone':zone,
+                        'quartal':quartal,
+                        'parcel':parcel
+                    }
+                })
+                .done(function(data2){
+                  if (data2.data && data2.data.length>0) {
+                    var res2 = data2.data[0];
+
+                    var content = ''
+                      + 'Кадастровый номер: ' + res2.cadnum + '<br/>' 
+                      + 'Использование: ' +  res2.use + '<br/>' 
+                      + 'Площадь: ' + res2.area + ' ' + res2.unit_area + '<br/>' 
+                      + 'Целевое назначение: ' + res2.purpose;
+     
+                    dfdid.resolve({
+                        content : content
+                        , latlng : latlng
+                        , layer: poly 
+                    });
+                  }
+
+                });
+            }
+        });
+
+        return dfdid.promise();
+    }
+
+    var layer = new (L.GeoManager.LayersFactory.createSimpleIndentifyLayer(identify))();
+
+    var geomanager={
+        options: options
+    };
+
+    layer.geomanager = geomanager;
+
+    dfd.resolve(
+        layer
+    )
+
+    return dfd.promise();
+}
+
 L.GeoManager.UACadastreGeocode = function (options) {
 
     var that=this
     , cadnum = options.query
     , cadparts = cadnum.split(':')
     , cadjoin = cadparts.join('')
-    //, bounds = arg.bounds
-    , unproject = function (x, y) {
-        var earthRadius = 6378137;
-        return L.CRS.EPSG900913.projection.unproject((new L.Point(x, y)).divideBy(earthRadius));
-    }
     , ajaxopt
     , ajaxtype
     , zoom;
@@ -38,8 +149,9 @@ L.GeoManager.UACadastreGeocode = function (options) {
                 , koatuu = cadnumparts[0]
                 , zone = cadnumparts[1]
                 , quartal = cadnumparts[2]
-                , parcel = cadnumparts[3]
-                , ajaxopt2 = {       
+                , parcel = cadnumparts[3];
+                
+                $.ajax({       
                     url : 'http://212.26.144.110/kadastrova-karta/get-parcel-Info'
                     , type: 'GET'
                     , dataType : 'json'
@@ -49,9 +161,7 @@ L.GeoManager.UACadastreGeocode = function (options) {
                         'quartal':quartal,
                         'parcel':parcel
                     }
-                };
-                
-                $.ajax(ajaxopt2)
+                })
                 .done(function(data2){
                   if (data2.data && data2.data.length>0) {
                     var res2 = data2.data[0];
